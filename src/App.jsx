1485e16f7b0f3c2ab0f20def4e78d33d6331cfd4
ErikVillarreal-bit <eriksamuel76@gmail.com>
2023-04-19 11:09:00 -0600

    button reset added

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ const App = () => {
   const [squares, setSquares] = useState(Array(9).fill(null))
 
   const handleClick = (i) => {
-    console.log("Valor de i", i)
     if (squares[i] || calculateWinner(squares)) {
       return
     }
@@ -22,7 +21,13 @@ const App = () => {
 
   }
 
+  const resetGame = () => {
+    setXIsNext(true)
+    setSquares(Array(9).fill(null))
+  }
+
   const winner = calculateWinner(squares)
+  console.log(winner)
   let status
   if (winner) {
     status = "Winner: " + winner
@@ -39,7 +44,10 @@ const App = () => {
     items-center
     justify-center
     ">
-      <h3 className="font-sans text-white font-semibold my-5 text-2xl mx-5">{status}</h3>
+      <div className="flex justify-around items-center">
+        <h3 className="font-sans text-white font-semibold my-5 text-2xl mx-5">{status}</h3>
+        {winner ? <button className="mb-2 bg-white text-slate-900 rounded-md w-28 h-10 hover:bg-slate-200" onClick={resetGame}>New game</button> : <button className="mb-2 bg-white text-slate-900 rounded-md w-28 h-10 hover:bg-slate-200" onClick={resetGame}>Reset</button>}
+      </div>
       <div className="flex">
         <div>
           <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
